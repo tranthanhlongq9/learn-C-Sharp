a7ba4b00abe83b60e68a16309fe0e6a4604be47d
Tr Thanh Long <you@example.com>
2021-05-28 15:49:22 +0700

    Update 28/5

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        /// <summary>
+        ///     phương thức main của class HelloWorld
+        ///     
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //in dòng chữ hello world lên màn hình
+            Console.WriteLine("Hello World");
+            //dừng màn hình để xem kết quả
+            Console.ReadKey();
+
+            //sẻ ko in aa ra màn hình vì ReadKey đã dừng màn hình.
+            Console.WriteLine("aa");
+        }
+    }
+}
