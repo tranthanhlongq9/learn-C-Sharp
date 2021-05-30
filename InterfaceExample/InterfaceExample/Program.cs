4ff5f9e05613635566817b5598f2b86994bfed31
Thanh Long <tranthanhlong.q9@gmail.com>
2021-05-30 20:04:51 +0700

    update 30/5

---
+++ InterfaceExample/InterfaceExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            double total = product.GetTotal(50000, 10);
+            Console.WriteLine("Total of order: " + total);
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IProduct //interface : 1 - luôn là public
+                                  // 2 - không định nghĩa đc thân hàm
+                                  // 3 - 1 class có thể kế thừa nhìu interface
+    {
+        double GetTotal(double price, int quantity);
+
+    }
+    class Product : IProduct
+    {
+        public double GetTotal(double price, int quantity)
+        {
+            return price * quantity;
+        }
+    }
+
+}
